bc1f22289ff2ba31bddd132d8a8a12472dac6655
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-04-21 01:24:17 +0600

    setda from json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [user,setUser]=useState([])
+  useEffect(() =>{
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+    .then(res =>res.json())
+    .then(data => setUser(data))
+  },[])
   return (
     <div className="App">
-     
+     <h1>{user.length}</h1>
     </div>
   );
 }
